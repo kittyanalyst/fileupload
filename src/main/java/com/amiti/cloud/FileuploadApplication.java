0c33dd77e09f2d7a56cefd5680ce0f489d7bd7e0
Ramakrishna Chalasani <ramakrishna.chalasani.ctr@excella.com>
2016-11-02 08:50:40 -0400

    Added Flyway db migration support

--- src/main/java/com/amiti/cloud/FileuploadApplication.java
+++ src/main/java/com/amiti/cloud/FileuploadApplication.java
@@ -1,11 +1,18 @@
 package com.amiti.cloud;
 
+import com.amiti.cloud.platform.config.Profiles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication
+@Profile(Profiles.APPLICATION)
+@SpringBootApplication (exclude = {
+		FlywayAutoConfiguration.class
+})
+
 public class FileuploadApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(FileuploadApplication.class);
